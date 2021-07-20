8e14d99f7ba92d37043e4df4293820bf10a77865
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-07-20 19:55:36 +0300

    Implement tests Setup

--- src/Net.FuncServiceOrchestrator.Tests/AsyncFuncServiceOrchestratorTests.TestInvokeAsync.cs
+++ src/Net.FuncServiceOrchestrator.Tests/AsyncFuncServiceOrchestratorTests.TestInvokeAsync.cs
@@ -1,7 +1,6 @@
 #nullable enable
 
 using NUnit.Framework;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Net.FuncServiceOrchestrator.Tests
@@ -21,53 +20,17 @@ namespace System.Net.FuncServiceOrchestrator.Tests
             int y,
             int expectedResult)
         {
-            var orchestra = await CreateOrchestratorAsync(a, b, c, d, e, x, y, cancellationToken: default);
+            await serviceA.SetLinearSourceAsync(a, cancellationToken: default);
+            await serviceB.SetLinearSourceAsync(b, cancellationToken: default);
+            await serviceC.SetLinearSourceAsync(c, cancellationToken: default);
+            await serviceD.SetLinearSourceAsync(d, cancellationToken: default);
+            await serviceE.SetLinearSourceAsync(e, cancellationToken: default);
+            await serviceX.SetLinearSourceAsync(x, cancellationToken: default);
+            await serviceY.SetLinearSourceAsync(y, cancellationToken: default);
 
             var actualResult = await orchestra.InvokeAsync(cancellationToken: default);
 
             Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        private static async ValueTask<IAsyncFuncServiceOrchestrator<int>> CreateOrchestratorAsync(
-            int a,
-            int b,
-            int c,
-            int d,
-            int e,
-            int x,
-            int y,
-            CancellationToken cancellationToken)
-        {
-            var serviceA = AsyncFuncService.CreateLinear<int>("A");
-            await serviceA.SetLinearSourceAsync(a, cancellationToken);
-
-            var serviceB = AsyncFuncService.CreateLinear<int>("B");
-            await serviceB.SetLinearSourceAsync(b, cancellationToken);
-
-            var serviceC = AsyncFuncService.CreateLinear<int>("C");
-            await serviceC.SetLinearSourceAsync(c, cancellationToken);
-
-            var serviceD = AsyncFuncService.CreateLinear<int>("D");
-            await serviceD.SetLinearSourceAsync(d, cancellationToken);
-
-            var serviceE = AsyncFuncService.CreateLinear<int>("E");
-            await serviceE.SetLinearSourceAsync(e, cancellationToken);
-
-            var serviceX = AsyncFuncService.CreateLinear<int>("X");
-            await serviceX.SetLinearSourceAsync(x, cancellationToken);
-
-            var serviceY = AsyncFuncService.CreateLinear<int>("Y");
-            await serviceY.SetLinearSourceAsync(y, cancellationToken);
-
-            var serviceFx = AsyncFuncService.Create("Fx", new[] { serviceX }, new FuncFx());
-            var serviceFy = AsyncFuncService.Create("Fy", new[] { serviceY }, new FuncFy());
-            var serviceFab = AsyncFuncService.Create("Fab", new[] { serviceA, serviceB }, new FuncFab());
-            var serviceFcd = AsyncFuncService.Create("Fcd", new[] { serviceC, serviceD, serviceFab, serviceFy }, new FuncFcd());
-            var serviceFe = AsyncFuncService.Create("Fe", new[] { serviceE, serviceFcd, serviceFx }, new FuncFe());
-
-            var serviceFr = AsyncFuncService.Create("Fr", new[] { serviceFe }, new FuncFr());
-
-            return await AsyncFuncServiceOrchestrator.CreateAsync(serviceFr, cancellationToken);
         }
     }
 }
