ed4717dc0fd48fc31eefbeac78d3a16f7f7a0393
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-07-22 13:36:54 +0300

    Refactor

--- src/Net.FuncServiceOrchestrator/Implementation/AsyncFuncServiceOrchestrator.Func.Strategy.Singleton.cs
+++ src/Net.FuncServiceOrchestrator/Implementation/AsyncFuncServiceOrchestrator.Func.Strategy.Singleton.cs
@@ -7,18 +7,18 @@ namespace System.Net
 {
     partial class AsyncFuncServiceOrchestrator<TValue>
     {
-        private async ValueTask<TValue> InvokeSingletonAsync(CancellationToken cancellationToken)
+        private ValueTask<TValue> InvokeSingletonAsync(CancellationToken cancellationToken)
         {
             #region Check if the task is canceled
 
             if (cancellationToken.IsCancellationRequested)
             {
-                return await ValueTask.FromCanceled<TValue>(cancellationToken).ConfigureAwait(false);
+                return ValueTask.FromCanceled<TValue>(cancellationToken);
             }
 
             #endregion
 
-            return await root.InvokeAsync(cancellationToken).ConfigureAwait(false);
+            return root.InvokeAsync(cancellationToken);
         }
     }
 }
